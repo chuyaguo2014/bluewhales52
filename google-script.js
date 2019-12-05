6d047b516c58c1db9f357c1f2617a4599a2ac94a
Chuya Guo <chuya.guo@gmail.com>
2019-12-05 13:37:24 -0500

    added comments - so far everything works

--- google-script.js
+++ google-script.js
@@ -1,9 +1,15 @@
+/**
+ * This is the main/entry point of the script. 
+ * We take a spreadsheet containing a sheet of form responses and a sheet of student names, 
+ * calculates for each student, how many other students have chosen him/her/them in their responses
+ * and saves the report in the sheet called "Result"
+ */
 function processFormResponses() {
     try {
         var spreadsheet = getSpreadsheet("1d-ti3j4MfBt9MmhI_zRLob9wMokfCuIRs2HMcmuSkVY");
         var studentList = getStudentList(spreadsheet);
         var responseSheet = getSheet(spreadsheet, "Form Response");
-        var numResponse = getNumberOfResponse(responseSheet);
+        var numResponse = getNumberOfResponses(responseSheet);
 
         var lunchDateResult = initializeResult(studentList);
 
@@ -12,30 +18,54 @@ function processFormResponses() {
             var votes = response.split(",");
             votes.forEach(function (vote) {
                 var name = vote.trim();
-                var old = lunchDateResult[name];
                 lunchDateResult[name] = lunchDateResult[name] + 1;
             });
         }
-        Logger.log("this is my result: \n\n");
-        Logger.log(lunchDateResult);
+        log("final result: ", lunchDateResult);
     }
     catch (e) {
-        Logger.log("Error occurred!");
-        Logger.log(e);
+        log("error occurred: ", e);
     }
     finally {
-        Logger.log("End of script. Bye!");
+        log("end of script. Good bye!");
     }
 }
 
+/**
+ * @param  {string} string - the string to describe what you are about to log
+ * @param  {object} obj - the actual object to log
+ */
+function log(string, obj) {
+    if (obj == undefined) {
+        Logger.log(string);
+    }
+    else {
+        Logger.log(string);
+        Logger.log(obj);
+    }
+}
+
+/**
+ * @param  {string} spreadsheetID - the id of the spreadsheet
+ * @returns the google spreadsheet 
+ */
 function getSpreadsheet(spreadsheetID) {
     return SpreadsheetApp.openById(spreadsheetID);
 }
 
+/**
+ * @param  {object} spreadsheet - the google spreadsheet
+ * @param  {string} sheetName - the name of the sheet within the spreadsheet
+ */
 function getSheet(spreadsheet, sheetName) {
     return spreadsheet.getSheetByName(sheetName);
 }
 
+/**
+ * @param  {obj} spreadsheet - the google spreadsheet containing a sheet named "Names"; 
+ * inside the sheet, the A1 cell has value "Full Name"; the subsequent cells in column A has the student names
+ * @returns {array} an array of strings containing student names
+ */
 function getStudentList(spreadsheet) {
     var sheet = getSheet(spreadsheet, "Names");
     var values = sheet.getRange("A2:A").getValues(); // since A1 is literally "Full Name"
@@ -44,6 +74,10 @@ function getStudentList(spreadsheet) {
     return cleanArray(rawList);
 }
 
+/**
+ * @param  {array} values - the input array
+ * @returns the number of meaningful/non-empty elements in the array
+ */
 function getNumberOfNonEmptyCells(values) {
     var total = 0;
     for (var i = 0; i < values.length; i++) {
@@ -54,6 +88,13 @@ function getNumberOfNonEmptyCells(values) {
     return total;
 }
 
+/**
+ * Cleans the input and bring all the nested array elements out
+ * @param  {array} rawList - an array returned by getValues() method, whose elements are also single-element arrays
+ * Example: [['coffee'], ['cupcakes']]
+ * @returns an array of the same order but without the nested elements;
+ * using the example above the result will be ['coffee', 'cupcakes']
+ */
 function cleanArray(rawList) {
     var result = [];
     for (var i = 0; i < rawList.length; i++) {
@@ -62,7 +103,12 @@ function cleanArray(rawList) {
     return result;
 }
 
-function getNumberOfResponse(sheet) {
+/**
+ * @param  {} sheet - the sheet (not the whole spreadsheet) containing the form responses
+ * @returns {number} the number of responses that have been submitted
+ * @todo parameterize the timestamp
+ */
+function getNumberOfResponses(sheet) {
     var values = sheet.getRange("B2:B").getValues(); // since B1 is the question itself
     return getNumberOfNonEmptyCells(values);
 }
@@ -71,6 +117,11 @@ function getIndividualResponse(sheet, columnName, rowIndex) {
     return sheet.getRange(columnName).getValues()[0][rowIndex];
 }
 
+/**
+ * Initializes an array to store all the student names and how many votes they each got (initially 0 for all)
+ * @param  {Array} studentList - an array of strings of student names
+ * @returns {object} - an object where student name (string) is the key and the value is 0
+ */
 function initializeResult(studentList) {
     var result = {};
     for (var i = 0; i < studentList.length; i++) {
